0d94d79574340723262671f648cff67f294c355a
wheatandcat <wheatandcat@gmail.com>
2026-02-25 23:19:22 +0900

    fix

--- RssFeedReader/RssFeedReader/RssFeedReaderApp.swift
+++ RssFeedReader/RssFeedReader/RssFeedReaderApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RssFeedReaderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().frame(minWidth: 800, minHeight: 800)
         }
     }
 }
